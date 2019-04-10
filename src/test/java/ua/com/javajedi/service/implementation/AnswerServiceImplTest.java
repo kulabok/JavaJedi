82c72dffb9638bbdc05f0a14b71d811ab34a792e
vbols <vbols@softserveinc.com>
2019-04-10 12:06:42 +0300

    Commit with empty test to check if jenkins works.

---
+++ src/test/java/ua/com/javajedi/service/implementation/AnswerServiceImplTest.java
@@ -0,0 +1,46 @@
+package ua.com.javajedi.service.implementation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnswerServiceImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testShouldAddAnswer() {
+		assert true;
+	}
+
+	@Test
+	public void update() {
+		assert true;
+	}
+
+	@Test
+	public void delete() {
+	}
+
+	@Test
+	public void findById() {
+	}
+
+	@Test
+	public void findAll() {
+	}
+
+	@Test
+	public void findAllByExerciseId() {
+	}
+
+	@Test
+	public void resolveAnswer() {
+	}
+}
